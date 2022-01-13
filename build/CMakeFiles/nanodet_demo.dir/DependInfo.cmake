5b2a70d7f74e331797f0d4dd46aa07d21132a10c
alexrog <acrogers307@gmail.com>
2022-01-13 13:48:02 -0500

    Initial commit

---
+++ build/CMakeFiles/nanodet_demo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/alexrogers/Documents/GitHub/nanodet/demo_openvino/main.cpp" "CMakeFiles/nanodet_demo.dir/main.cpp.o" "gcc" "CMakeFiles/nanodet_demo.dir/main.cpp.o.d"
+  "/Users/alexrogers/Documents/GitHub/nanodet/demo_openvino/nanodet_openvino.cpp" "CMakeFiles/nanodet_demo.dir/nanodet_openvino.cpp.o" "gcc" "CMakeFiles/nanodet_demo.dir/nanodet_openvino.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
